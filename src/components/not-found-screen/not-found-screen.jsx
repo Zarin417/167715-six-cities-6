33fec0a273eb1e1151a91c941d62ecb2d12ab5c8
Evgeny Vorona <zarin417@gmail.com>
2021-03-06 16:02:49 +0200

    Добавляет реализацию маршрутизации страницы, страницу для 404

---
+++ src/components/not-found-screen/not-found-screen.jsx
@@ -0,0 +1,97 @@
+import React from "react";
+
+const NotFoundScreen = () => {
+  return (
+    <div className='not-found-container'>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>4</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <span className='particle'>0</span>
+      <article className='content'>
+        <p>Шеф! Все пропало!</p>
+        <p>Ошибка <strong>404</strong>. Ничего не найдено</p>
+        <p>
+          <button>Вернуться назад</button>
+        </p>
+      </article>
+    </div>
+  );
+};
+
+export default NotFoundScreen;
